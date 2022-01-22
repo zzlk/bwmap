2c1f21612691778c055885e8ce36e5e7a7944f64
zzlk <2418897+zzlk@users.noreply.github.com>
2022-01-22 07:04:10 -0800

    move integration tests to their own module

--- src/util.rs
+++ src/util.rs
@@ -10,19 +10,6 @@ pub(crate) fn parse_slice<T: Copy>(s: &[u8]) -> T {
     unsafe { *(s.as_ptr() as *const T) }
 }
 
-pub(crate) fn parse_null_terminated_string(s: &[i8]) -> &str {
-    let mut index = 0;
-    for &c in s {
-        if c == 0i8 {
-            break;
-        }
-
-        index += 1;
-    }
-
-    std::str::from_utf8(reinterpret_slice2(&s[0..index])).unwrap()
-}
-
 pub(crate) fn reinterpret_slice<T: Copy + Sized>(s: &[u8]) -> &[T] {
     if s.len() % std::mem::size_of::<T>() != 0 {
         panic!();
@@ -33,12 +20,25 @@ pub(crate) fn reinterpret_slice<T: Copy + Sized>(s: &[u8]) -> &[T] {
     }
 }
 
-pub(crate) fn reinterpret_slice2<T: Copy + Sized>(s: &[i8]) -> &[T] {
-    if s.len() % std::mem::size_of::<T>() != 0 {
-        panic!();
-    }
+// pub(crate) fn parse_null_terminated_string(s: &[i8]) -> &str {
+//     let mut index = 0;
+//     for &c in s {
+//         if c == 0i8 {
+//             break;
+//         }
 
-    unsafe {
-        std::slice::from_raw_parts(s.as_ptr() as *const T, s.len() / std::mem::size_of::<T>())
-    }
-}
+//         index += 1;
+//     }
+
+//     std::str::from_utf8(reinterpret_slice2(&s[0..index])).unwrap()
+// }
+
+// pub(crate) fn reinterpret_slice2<T: Copy + Sized>(s: &[i8]) -> &[T] {
+//     if s.len() % std::mem::size_of::<T>() != 0 {
+//         panic!();
+//     }
+
+//     unsafe {
+//         std::slice::from_raw_parts(s.as_ptr() as *const T, s.len() / std::mem::size_of::<T>())
+//     }
+// }
