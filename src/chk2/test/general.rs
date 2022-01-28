99b470eec7a472a2af870f3634c5c2785c81c4ac
zzlk <2418897+zzlk@users.noreply.github.com>
2022-01-28 14:16:34 -0800

    add test that downloads maps from bounding.net and parses them

--- src/chk2/test/general.rs
+++ src/chk2/test/general.rs
@@ -1,4 +1,6 @@
-use crate::{chk::ChunkName, merge_raw_chunks, parse_chk, parse_merged_chunks};
+use crate::{
+    chk::ChunkName, get_chk_from_mpq_in_memory, merge_raw_chunks, parse_chk, parse_merged_chunks,
+};
 use rayon::prelude::*;
 
 fn for_all_test_maps<F: Fn(walkdir::DirEntry) + Sync>(func: F) {
@@ -54,4 +56,74 @@ fn test_parse_merged_chunks() {
             parsed_chunks
         );
     });
+}
+
+#[test]
+fn test_a_lot_of_maps_from_bounding_dot_net() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let client = reqwest::Client::builder()
+        .user_agent("norecord")
+        .build()
+        .unwrap();
+
+    let hashes = {
+        let response = rt
+            .block_on(
+                client
+                    .request(
+                        reqwest::Method::GET,
+                        "https://bounding.net/api/tests/all_maps",
+                    )
+                    .header("cookie", "dontratelimitmebro=true")
+                    .send(),
+            )
+            .unwrap();
+
+        let json: Vec<String> = rt.block_on(response.json()).unwrap();
+
+        json
+    };
+
+    let ret: Vec<_> = hashes
+        .into_par_iter()
+        .map(|hash| (hash, client.clone()))
+        .map(|(hash, client)| async move {
+            client
+                .request(
+                    reqwest::Method::GET,
+                    format!("https://bounding.net/api/maps/{hash}"),
+                )
+                .header("cookie", "dontratelimitmebro=true")
+                .send()
+                .await
+                .unwrap()
+                .bytes()
+                .await
+                .unwrap()
+        })
+        .map(|mpq| async {
+            let chk_data = get_chk_from_mpq_in_memory(mpq.await.as_ref()).unwrap();
+            let raw_chunks = parse_chk(&chk_data);
+            let merged_chunks = merge_raw_chunks(&raw_chunks);
+            let parsed_chunks = parse_merged_chunks(&merged_chunks);
+
+            assert!(parsed_chunks.is_ok(), "{:?}", parsed_chunks);
+
+            let parsed_chunks = parsed_chunks.unwrap();
+
+            assert!(
+                parsed_chunks.get(&ChunkName::VCOD).is_some(),
+                "{:?}",
+                parsed_chunks
+            );
+        })
+        .collect();
+
+    rt.block_on(futures::future::join_all(ret));
+
+    rt.shutdown_background();
 }
