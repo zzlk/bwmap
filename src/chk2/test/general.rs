8b9b09ec4a89c5494898df305046f8c05c5fd173
zzlk <2418897+zzlk@users.noreply.github.com>
2022-01-31 10:40:41 -0800

    add tests for SMC and SMLP protectors. Implement huge hack while waiting for zezula's response on how to read correct chk file. Huge hack is just enumerating all files in mpq and picking the first chk-looking file

--- src/chk2/test/general.rs
+++ src/chk2/test/general.rs
@@ -28,7 +28,7 @@ fn for_all_test_maps<F: Fn(DirEntry) + Sync>(func: F) {
         })
         .count();
 
-    assert_eq!(processed_maps, 178);
+    assert_eq!(processed_maps, 179);
 }
 
 #[test]
@@ -50,8 +50,6 @@ fn test_parse_merged_chunks() {
 
         let parsed_chunks = parsed_chunks.unwrap();
 
-        // println!("{:?}", parsed_chunks);
-
         assert!(
             parsed_chunks.get(&ChunkName::VCOD).is_some(),
             "filename: {}, chk_data.len(): {}, {:?}",
@@ -60,6 +58,38 @@ fn test_parse_merged_chunks() {
             parsed_chunks
         );
     });
+}
+
+#[test]
+fn test_specific_map_protected_by_smc_version_2_dot_9() {
+    // SMC V2.9
+    let chk = crate::get_chk_from_mpq_filename(format!(
+        "{}/test_vectors/Sniper - Seed vA.scx",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+    .unwrap();
+
+    let raw_chunks = parse_chk(&chk);
+    let merged_chunks = merge_raw_chunks(&raw_chunks);
+    let parsed_chunks = parse_merged_chunks(&merged_chunks).unwrap();
+
+    assert!(parsed_chunks.get(&ChunkName::VCOD).is_some());
+}
+
+#[test]
+fn test_specific_map_protected_by_smlp_version_2_dot_5_dot_00() {
+    // SMLP 2.5.00
+    let chk = crate::get_chk_from_mpq_filename(format!(
+        "{}/test_vectors/Lnm Series Bound 12(p).scx",
+        env!("CARGO_MANIFEST_DIR")
+    ))
+    .unwrap();
+
+    let raw_chunks = parse_chk(&chk);
+    let merged_chunks = merge_raw_chunks(&raw_chunks);
+    let parsed_chunks = parse_merged_chunks(&merged_chunks).unwrap();
+
+    assert!(parsed_chunks.get(&ChunkName::VCOD).is_some());
 }
 
 #[test]
--- src/mpq.rs
+++ src/mpq.rs
@@ -1,3 +1,5 @@
+use crate::chk::verify_is_valid_chk;
+
 pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, anyhow::Error> {
     // This is really not the rust way to do things but stormlib_bindings is internally not threadsafe so what we can do.
     lazy_static::lazy_static! {
@@ -35,17 +37,23 @@ pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, an
             }
         };
 
-        let try_map_with_locale = |locale| {
+        let try_map_with_locale = |filename: &str, locale| {
+            println!("try_map_with_locale: {filename}, locale: {locale}");
+
             stormlib_bindings::SFileSetLocale(locale); // Set locale, this function never fails.
             let mut chk_data: Vec<u8> = vec![0; 32 * 1024 * 1024];
             let mut archive_file_handle = 0 as stormlib_bindings::HANDLE;
             if stormlib_bindings::SFileOpenFileEx(
                 mpq_handle,
-                "staredit\\scenario.chk\0".as_ptr() as *mut _,
+                filename.as_ptr() as *mut _,
                 0,
                 &mut archive_file_handle as *mut _,
             ) == false
             {
+                println!(
+                    "SFileOpenFileEx. GetLastError: {}, filename: {filename}",
+                    stormlib_bindings::GetLastError()
+                );
                 return Err(anyhow::anyhow!(
                     "SFileOpenFileEx. GetLastError: {}, filename: {filename}",
                     stormlib_bindings::GetLastError()
@@ -77,6 +85,11 @@ pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, an
                 if last_error != stormlib_bindings::ERROR_HANDLE_EOF
                     || size == chk_data.len() as u32
                 {
+                    println!(
+                        "SFileReadFile. GetLastError: {}, filename: {filename}",
+                        last_error,
+                    );
+
                     return Err(anyhow::anyhow!(
                         "SFileReadFile. GetLastError: {}, filename: {filename}",
                         last_error,
@@ -96,15 +109,91 @@ pub fn get_chk_from_mpq_filename(filename: String) -> anyhow::Result<Vec<u8>, an
 
         // PROTECTION: Some maps put fake scenario.chk files at different locales. Try to find the real one by trying a lot of them.
         for locale in locales {
-            if let Ok(x) = try_map_with_locale(locale) {
+            if let Ok(x) = try_map_with_locale("staredit\\scenario.chk\0", locale) {
                 // PROTECTION: Some maps put very small scenario.chk files in the mpq at different locales, ignore them.
-                if x.len() > 10 {
+                if verify_is_valid_chk(x.as_slice()) {
                     return Ok(x);
                 }
             }
         }
 
-        try_map_with_locale(0)
+        // If none of the files found are useful then fall back to enumarating every single file in the MPQ and check to see if it is a CHK file.
+        {
+            let mut find_data: stormlib_bindings::SFILE_FIND_DATA =
+                stormlib_bindings::SFILE_FIND_DATA {
+                    cFileName: [0i8; 1024],
+                    szPlainName: 0 as *mut i8,
+                    dwHashIndex: 0,
+                    dwBlockIndex: 0,
+                    dwFileSize: 0,
+                    dwFileFlags: 0,
+                    dwCompSize: 0,
+                    dwFileTimeLo: 0,
+                    dwFileTimeHi: 0,
+                    lcLocale: 0,
+                };
+
+            let search_handle = stormlib_bindings::SFileFindFirstFile(
+                mpq_handle,
+                format!("{}\0", "*").as_ptr() as *const i8,
+                &mut find_data as *mut stormlib_bindings::SFILE_FIND_DATA,
+                0 as *mut _,
+            );
+
+            scopeguard::defer! {
+                if stormlib_bindings::SFileFindClose(search_handle) == false {
+                    println!(
+                        "{:?}",
+                        anyhow::anyhow!(
+                            "SFileFindClose. GetLastError: {}, filename: {filename}",
+                            stormlib_bindings::GetLastError()
+                        )
+                    );
+                }
+            }
+
+            loop {
+                if search_handle == 0 as stormlib_bindings::HANDLE {
+                    return Err(anyhow::anyhow!(
+                        "search_handle == 0. No more files in MPQ. GetLastError: {}",
+                        stormlib_bindings::GetLastError()
+                    ));
+                }
+
+                let new_filename =
+                    crate::util::parse_null_terminated_string(&find_data.cFileName[0..]);
+
+                println!(
+                    "Enumarating Files: cFileName: {}, dwBlockIndex: {}, dwCompSize: {}, dwFileSize: {}, dwFileTimeHi: {}, dwFileTimeLo: {}, dwHashIndex: {}, lcLocale: {}",
+                    new_filename,
+                    find_data.dwBlockIndex,
+                    find_data.dwCompSize,
+                    find_data.dwFileSize,
+                    find_data.dwFileTimeHi,
+                    find_data.dwFileTimeLo,
+                    find_data.dwHashIndex,
+                    find_data.lcLocale,
+                );
+
+                if let Ok(x) = try_map_with_locale(new_filename, find_data.lcLocale) {
+                    // PROTECTION: Some maps put very small scenario.chk files in the mpq at different locales, ignore them.
+                    if verify_is_valid_chk(x.as_slice()) {
+                        return Ok(x);
+                    }
+                }
+
+                if stormlib_bindings::SFileFindNextFile(
+                    search_handle,
+                    &mut find_data as *mut stormlib_bindings::SFILE_FIND_DATA,
+                ) == false
+                {
+                    return Err(anyhow::anyhow!(
+                        "SFileFindNextFile. No more files in MPQ. GetLastError: {}",
+                        stormlib_bindings::GetLastError()
+                    ));
+                }
+            }
+        }
     }
 }
 
