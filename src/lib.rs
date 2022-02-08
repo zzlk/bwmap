efdd91e520ce31ca768c055c8f6beb7fd987118e
zzlk <2418897+zzlk@users.noreply.github.com>
2022-02-08 11:54:56 -0800

    expose more trigger types

--- src/lib.rs
+++ src/lib.rs
@@ -18,3 +18,8 @@ pub use chk::ParsedChunk;
 pub use mpq::get_chk_from_mpq_filename;
 pub use mpq::get_chk_from_mpq_in_memory;
 pub use trig::parse_triggers;
+pub use trig::Action;
+pub use trig::Condition;
+pub use trig::Group;
+pub use trig::Trigger;
+pub use trig::UnitType;
