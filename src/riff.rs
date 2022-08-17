529fcc8b8c3daed13621df62e85a637706f8d10a
zzlk <2418897+zzlk@users.noreply.github.com>
2022-08-16 18:12:16 -0700

    add tests to riff parser

--- src/riff.rs
+++ src/riff.rs
@@ -1,11 +1,12 @@
 use crate::util::parse_slice;
 use tracing::instrument;
 
+#[derive(Debug)]
 pub struct RiffChunk<'a> {
-    fourcc: [u8; 4],
-    size: u32,
-    offset: usize,
-    data: &'a [u8],
+    pub fourcc: [u8; 4],
+    pub size: u32,
+    pub offset: usize,
+    pub data: &'a [u8],
 }
 
 #[instrument(level = "trace", skip_all)]
@@ -38,4 +39,51 @@ pub fn parse_riff(chk: &[u8]) -> Vec<RiffChunk> {
     }
 
     ret
+}
+
+#[cfg(test)]
+mod test {
+    use crate::riff::parse_riff;
+    use anyhow::Result;
+    use walkdir::{DirEntry, WalkDir};
+
+    fn for_all_test_maps<F: Fn(DirEntry)>(func: F) {
+        let processed_maps = WalkDir::new(format!("{}/test_vectors", env!("CARGO_MANIFEST_DIR")))
+            .into_iter()
+            .filter_map(Result::ok)
+            .filter(
+                |e| match e.file_name().to_string_lossy().to_string().as_str() {
+                    "[EUD]컴디 파이널.scx" => false,
+                    "마인의 폭피 1.scm" => false,
+                    _ => {
+                        !e.file_type().is_dir()
+                            && (e.file_name().to_string_lossy().ends_with(".scx")
+                                || e.file_name().to_string_lossy().ends_with(".scm"))
+                    }
+                },
+            )
+            .map(|e| {
+                func(e);
+            })
+            .count();
+
+        assert_eq!(processed_maps, 185);
+    }
+
+    #[test]
+    fn test_parse_riff() {
+        for_all_test_maps(|e| {
+            println!("file: {}", e.file_name().to_string_lossy());
+            let chk_data =
+                crate::get_chk_from_mpq_filename(e.path().to_string_lossy().to_string()).unwrap();
+
+            let riff_chunks = parse_riff(&chk_data);
+
+            assert!(riff_chunks.len() > 0);
+            assert!(riff_chunks
+                .iter()
+                .position(|x| x.fourcc == *b"VER ")
+                .is_some());
+        });
+    }
 }
